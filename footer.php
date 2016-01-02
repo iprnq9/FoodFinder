68e0e00934968e6d22c745f4356231b765bf09a6
Ian <ian.roberts@mst.edu>
2016-01-02 17:59:53 -0600

    Edited Footer php file
    No changes really

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="page-footer green hide-on-small-only">
+  <div class="container">
+    <div class="row">
+      <div class="col s8 offset-s2 m4 offset-m4 l2 offset-l5">
+        <img src="images/foodFinderLogo3.png" class="responsive-img right" />
+      </div>
+  </div>
+</footer>
