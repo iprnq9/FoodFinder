b3d03119e4a5331287343c7fa1382d750cabaaba
Ian <ian.roberts@mst.edu>
2016-01-02 17:45:00 -0600

    Added PHP header, footer for test

---
+++ header.php
@@ -0,0 +1,20 @@
+<div class="navbar-fixed">
+  <nav class="green lighten-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo waves-effect waves-light"><img src="images/logo-icon.png" height="" />Food Finder</a>
+      <ul class="right hide-on-med-and-down">
+        <li class="active"><a href="#" class="waves-effect waves-light"><i class="material-icons left">local_dining</i>Chartwells Options</a></li>
+        <li><a href="#" class="waves-effect waves-light"><i class="material-icons left">local_atm</i>Food Fundraisers</a></li>
+        <li><a href="#" class="waves-effect waves-light"><i class="material-icons left">dashboard</i>Dashboard</a></li>
+        <li><a href="#" class="waves-effect waves-light"><i class="material-icons left">settings</i>Manage</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li class="active"><a href="#"><i class="material-icons left">local_dining</i>Chartwells Options</a></li>
+        <li><a href="#"><i class="material-icons left">local_atm</i>Food Fundraisers</a></li>
+        <li><a href="#"><i class="material-icons left">dashboard</i>Dashboard</a></li>
+        <li><a href="#"><i class="material-icons left">settings</i>Manage</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+</div>
