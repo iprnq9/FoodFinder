e9ed560cc21d78536310348ef7d2539dc9d8361e
Ian <ian.roberts@mst.edu>
2016-01-06 13:49:13 -0600

    Minor changes to table highlighting.

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="custom.css" rel="stylesheet">
   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
@@ -21,6 +22,92 @@
       <div class="row">
         <div class="col m1 l2">&nbsp;</div>
         <div class="col s12 m10 l8 offset-l2 offset-m1">
+          <div class="card hoverable">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="images/bagels.jpg">
+            </div>
+            <div class="card-content green lighten-3">
+              <span class="card-title activator grey-text text-darken-4"><i class="material-icons">event_available</i>Einstein Bros. Bagels<i class="material-icons right">add</i></span>
+              <p>A coffee shop for students serving delicious bagels and more.</p>
+            </div>
+            <div class="card-reveal green lighten-3">
+              <div class="row">
+                <span class="card-title grey-text text-darken-4"><i class="material-icons">event_available</i>Einstein Bros. Bagels<i class="material-icons right">close</i></span>
+                <p>A coffee shop for students serving delicious bagels and more.</p>
+              </div>
+              <div class="">
+                <div class="row">
+                  <div class="col s12 m12 l12">
+                    <table class="responsive-table centered bordered white">
+                      <thead><tr>
+                        <th>Day</th>
+                        <th>Open</th>
+                        <th>Closed</th>
+                        <th>Open</th>
+                        <th>Closed</th>
+                      </tr></thead>
+                      <tr class="day-1">
+                        <td>Monday</td>
+                        <td>7:00am</td>
+                        <td>12:00pm</td>
+                        <td>1:00pm</td>
+                        <td>7:00pm</td>
+                      </tr>
+                      <tr class="day-2">
+                        <td>Tuesday</td>
+                        <td>7:00am</td>
+                        <td>12:00pm</td>
+                        <td>1:00pm</td>
+                        <td>7:00pm</td>
+                      </tr>
+                      <tr class="day-3">
+                        <td>Wednesday</td>
+                        <td>7:00am</td>
+                        <td>12:00pm</td>
+                        <td>1:00pm</td>
+                        <td>7:00pm</td>
+                      </tr>
+                      <tr class="day-4">
+                        <td>Thursday</td>
+                        <td>7:00am</td>
+                        <td>12:00pm</td>
+                        <td>1:00pm</td>
+                        <td>7:00pm</td>
+                      </tr>
+                      <tr class="day-5">
+                        <td>Friday</td>
+                        <td>7:00am</td>
+                        <td>12:00pm</td>
+                        <td>1:00pm</td>
+                        <td>7:00pm</td>
+                      </tr>
+                      <tr class="day-6">
+                        <td>Saturday</td>
+                        <td>7:00am</td>
+                        <td>12:00pm</td>
+                        <td>1:00pm</td>
+                        <td>7:00pm</td>
+                      </tr>
+                      <tr class="day-0">
+                        <td>Sunday</td>
+                        <td>7:00am</td>
+                        <td>12:00pm</td>
+                        <td>1:00pm</td>
+                        <td>7:00pm</td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col s12 m12 l12 center-align">
+                    <a href="material-profile.html" class="waves-effect waves-light btn green center-align"><i class="material-icons left">person_pin</i>View Profile</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
           <div class="card hoverable">
             <div class="card-image waves-effect waves-block waves-light">
               <img class="activator" src="images/bagels.jpg">
@@ -43,37 +130,37 @@
                         <th>Open</th>
                         <th>Closed</th>
                       </tr></thead>
-                      <tr>
+                      <tr class="day-1">
                         <td>Monday</td>
                         <td>7:00am</td>
                         <td>7:00pm</td>
                       </tr>
-                      <tr>
+                      <tr class="day-2">
                         <td>Tuesday</td>
                         <td>7:00am</td>
                         <td>7:00pm</td>
                       </tr>
-                      <tr>
+                      <tr class="day-3">
                         <td>Wednesday</td>
                         <td>7:00am</td>
                         <td>7:00pm</td>
                       </tr>
-                      <tr>
+                      <tr class="day-4">
                         <td>Thursday</td>
                         <td>7:00am</td>
                         <td>7:00pm</td>
                       </tr>
-                      <tr>
+                      <tr class="day-5">
                         <td>Friday</td>
                         <td>7:00am</td>
                         <td>7:00pm</td>
                       </tr>
-                      <tr>
+                      <tr class="day-6">
                         <td>Saturday</td>
                         <td>7:00am</td>
                         <td>7:00pm</td>
                       </tr>
-                      <tr>
+                      <tr class="day-0">
                         <td>Sunday</td>
                         <td>7:00am</td>
                         <td>7:00pm</td>
@@ -91,19 +178,6 @@
           </div>
 
 
-          <div class="card hoverable">
-            <div class="card-image waves-effect waves-block waves-light">
-              <img class="activator" src="images/rustic.jpg">
-            </div>
-            <div class="card-content">
-              <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">add</i></span>
-              <p><a href="#">This is a link</a></p>
-            </div>
-            <div class="card-reveal">
-              <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-              <p>Here is some more information about this product that is only revealed once clicked on.</p>
-            </div>
-          </div>
         </div>
       </div>
     <br><br>
@@ -118,6 +192,16 @@
 
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script>
+    $(document).ready(function(){
+
+      var dateVar = new Date();
+      var n = dateVar.getDay();
+
+      $('tr.day-' + n).addClass('current-day');
+
+    });
+  </script>
   <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
 
