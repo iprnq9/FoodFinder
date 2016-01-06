86d938fca23468273a95765d43ec21095b94ac3b
Ian <ian.roberts@mst.edu>
2016-01-06 15:36:14 -0600

    PHP Tables
    Update in on central point rather than multiple places in html

---
+++ hours-tj.php
@@ -0,0 +1,58 @@
+<table class="responsive-table centered bordered white">
+  <thead><tr>
+    <th>Day</th>
+    <th>Open</th>
+    <th>Closed</th>
+    <th>Open</th>
+    <th>Closed</th>
+  </tr></thead>
+  <tr class="day-1">
+    <td>Monday</td>
+    <td>7:00am</td>
+    <td>12:00pm</td>
+    <td>1:00pm</td>
+    <td>7:00pm</td>
+  </tr>
+  <tr class="day-2">
+    <td>Tuesday</td>
+    <td>7:00am</td>
+    <td>12:00pm</td>
+    <td>1:00pm</td>
+    <td>7:00pm</td>
+  </tr>
+  <tr class="day-3">
+    <td>Wednesday</td>
+    <td>7:00am</td>
+    <td>12:00pm</td>
+    <td>1:00pm</td>
+    <td>7:00pm</td>
+  </tr>
+  <tr class="day-4">
+    <td>Thursday</td>
+    <td>7:00am</td>
+    <td>12:00pm</td>
+    <td>1:00pm</td>
+    <td>7:00pm</td>
+  </tr>
+  <tr class="day-5">
+    <td>Friday</td>
+    <td>7:00am</td>
+    <td>12:00pm</td>
+    <td>1:00pm</td>
+    <td>7:00pm</td>
+  </tr>
+  <tr class="day-6">
+    <td>Saturday</td>
+    <td>7:00am</td>
+    <td>12:00pm</td>
+    <td>1:00pm</td>
+    <td>7:00pm</td>
+  </tr>
+  <tr class="day-0">
+    <td>Sunday</td>
+    <td>7:00am</td>
+    <td>12:00pm</td>
+    <td>1:00pm</td>
+    <td>7:00pm</td>
+  </tr>
+</table>
--- index.php
+++ index.php
@@ -38,64 +38,7 @@
               <div class="">
                 <div class="row">
                   <div class="col s12 m12 l12">
-                    <table class="responsive-table centered bordered white">
-                      <thead><tr>
-                        <th>Day</th>
-                        <th>Open</th>
-                        <th>Closed</th>
-                        <th>Open</th>
-                        <th>Closed</th>
-                      </tr></thead>
-                      <tr class="day-1">
-                        <td>Monday</td>
-                        <td>7:00am</td>
-                        <td>12:00pm</td>
-                        <td>1:00pm</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-2">
-                        <td>Tuesday</td>
-                        <td>7:00am</td>
-                        <td>12:00pm</td>
-                        <td>1:00pm</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-3">
-                        <td>Wednesday</td>
-                        <td>7:00am</td>
-                        <td>12:00pm</td>
-                        <td>1:00pm</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-4">
-                        <td>Thursday</td>
-                        <td>7:00am</td>
-                        <td>12:00pm</td>
-                        <td>1:00pm</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-5">
-                        <td>Friday</td>
-                        <td>7:00am</td>
-                        <td>12:00pm</td>
-                        <td>1:00pm</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-6">
-                        <td>Saturday</td>
-                        <td>7:00am</td>
-                        <td>12:00pm</td>
-                        <td>1:00pm</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-0">
-                        <td>Sunday</td>
-                        <td>7:00am</td>
-                        <td>12:00pm</td>
-                        <td>1:00pm</td>
-                        <td>7:00pm</td>
-                      </tr>
-                    </table>
+                    <?php include 'hours-tj.php' ?>
                   </div>
                 </div>
                 <div class="row">
@@ -124,48 +67,7 @@
               <div class="">
                 <div class="row">
                   <div class="col s12 m12 l12">
-                    <table class="responsive-table centered bordered white">
-                      <thead><tr>
-                        <th>Day</th>
-                        <th>Open</th>
-                        <th>Closed</th>
-                      </tr></thead>
-                      <tr class="day-1">
-                        <td>Monday</td>
-                        <td>7:00am</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-2">
-                        <td>Tuesday</td>
-                        <td>7:00am</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-3">
-                        <td>Wednesday</td>
-                        <td>7:00am</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-4">
-                        <td>Thursday</td>
-                        <td>7:00am</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-5">
-                        <td>Friday</td>
-                        <td>7:00am</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-6">
-                        <td>Saturday</td>
-                        <td>7:00am</td>
-                        <td>7:00pm</td>
-                      </tr>
-                      <tr class="day-0">
-                        <td>Sunday</td>
-                        <td>7:00am</td>
-                        <td>7:00pm</td>
-                      </tr>
-                    </table>
+                    <?php include 'hours-einsteins.php' ?>
                   </div>
                 </div>
                 <div class="row">
