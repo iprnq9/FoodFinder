16ddd9705ab3cd682769b17af50086d87d03d379
Ian <ian.roberts@mst.edu>
2016-01-06 22:18:52 -0600

    MC

---
+++ css.php
@@ -0,0 +1,6 @@
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="custom.css" rel="stylesheet">
+<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+<script type="text/javascript" src="js/date.js"></script>
