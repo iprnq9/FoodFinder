c4e5b4afa60c95185aea26928ed026975dd8eb08
Ian <ian.roberts@mst.edu>
2016-01-07 21:08:16 -0600

    Well, it worked. But it's not displaying anything
    ... -.-

--- db-test.php
+++ db-test.php
@@ -19,7 +19,7 @@ if (!$conn)
 $sql = "SELECT location_id, location_name FROM locations";
 $result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0)
+if (mysqli_num_rows($result) >= 0)
 {
   // output data of each row
   while($row = mysqli_fetch_assoc($result))
