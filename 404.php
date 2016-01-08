de17719648acbe00fbc9e11ac9d7627ae47c21ab
Ian <ian.roberts@mst.edu>
2016-01-07 22:28:51 -0600

    Added a 404 page~!

---
+++ 404.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Error 404 | FoodFinder</title>
+
+  <!-- CSS  -->
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="custom.css" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+</head>
+<body style="background-color: #eeeeee;">
+
+  <?php include 'header.php' ?>
+
+  <div class="container">
+    <div class="section">
+      <div class="large card center-align">
+        <h1>Error 404<br>No food found here. :/</h1>
+        <h3>That was our bad probably. Sorry 'bout that.</h3><br>
+        <div class="center-align"><a href="index.php" class="waves-effect waves-light btn-large green center-align z-depth-2"><i class="material-icons left">home</i>Homepage</a></div>
+      </div>
+    </div>
+  </div>
+
+  <?php include 'footer.php' ?>
+  <!--  Scripts-->
+  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
