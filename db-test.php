6479dc9ad8e95e934e7fd801f79bbd5962975ff6
Ian <ian.roberts@mst.edu>
2016-01-11 15:47:11 -0600

    Troubleshooting

--- db-test.php
+++ db-test.php
@@ -3,6 +3,10 @@
 <body>
 
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+ini_set('html_errors', 'On');
+
 $servername = "localhost";
 $username = "root";
 $password = "FoodFinder";
