7a7c67ae238ce97f81f042ea650f7ebdaf87c250
Ian <ian.roberts@mst.edu>
2016-01-11 16:04:18 -0600

    Playing around with PHP echo

--- db-test.php
+++ db-test.php
@@ -29,7 +29,9 @@ if (mysqli_num_rows($result) > 0)
   // output data of each row
   while($row = mysqli_fetch_assoc($result))
   {
-    echo "id: " . $row["location_id"]. " - Name: " . $row["location_name"]. "<br>";
+    echo "<div class=\"card\"";
+    echo "<h1>ID: " . $row["location_id"]. "<br>Name: " . $row["location_name"]. "</h1>";
+    echo "</div>";
   }
 }
 
