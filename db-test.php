61d448e80c087eca5e4c4e5a22c478073c6d6ccd
Ian <ian.roberts@mst.edu>
2016-01-12 20:36:21 -0600

    Testing

--- db-test.php
+++ db-test.php
@@ -44,5 +44,49 @@ mysqli_close($conn);
 
 ?>
 
+<br><br>
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+ini_set('html_errors', 'On');
+
+$servername = "localhost";
+$username = "root";
+$password = "FoodFinder";
+$dbname = "foodfinders";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn)
+{
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM newlocations";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0)
+{
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result))
+  {
+    echo "<div class=\"card\">\n";
+    echo "<h1>ID: " . $row["location_id"]. "<br>Closes: " . $row["dinner_end"]. "</h1>\n";
+    echo "</div>";
+  }
+}
+
+else
+{
+  echo "0 results";
+}
+
+mysqli_close($conn);
+
+?>
+
 </body>
 </html>
