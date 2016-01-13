60ebf42dd8e7f25fbd2780feb09cfeb3389d8c8f
Ian <ian.roberts@mst.edu>
2016-01-13 13:14:25 -0600

    Testing

--- update-data.php
+++ update-data.php
@@ -7,71 +7,77 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 ini_set('html_errors', 'On');
 
-$servername = "localhost";
-$username = "root";
-$password = "FoodFinder";
-$dbname = "foodfinders";
+if(isset($_POST['update'])) {
+    $servername = "localhost";
+    $username = "root";
+    $password = "FoodFinder";
+    $dbname = "foodfinders";
 
-$location_id = $_POST['location_id'];
-$location_name = $_POST['location_name'];
+    $location_id = $_POST['location_id'];
+    $location_name = $_POST['location_name'];
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
-if (!$conn)
-{
-    die("Connection failed: " . mysqli_connect_error());
-}
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-$sql = "UPDATE locations SET location_name = $new_name WHERE location_id = $location_id";
-$result = mysqli_query($conn, $sql);
+    $sql = "UPDATE locations SET location_name = $location_name WHERE location_id = $location_id";
+    $result = mysqli_query($conn, $sql);
 
-if (!$result)
-{
-    die('Could not update data: ' . mysqli_error($result));
+    if (!$result)
+    {
+        die('Could not update data: ' . mysqli_error($result));
+    }
+
+    else
+    {
+        echo "Updated successfully!";
+    }
+
+    mysqli_close($conn);
 }
 
 else
 {
-    echo "Updated successfully!";
+    ?>
+    <form method = "post" action = "<?php $_PHP_SELF ?>">
+        <table width = "400" border =" 0" cellspacing = "1"
+               cellpadding = "2">
+
+            <tr>
+                <td width = "100">Employee ID</td>
+                <td><input name = "location_id" type = "text"
+                           id = "location_id"></td>
+            </tr>
+
+            <tr>
+                <td width = "100">Employee Salary</td>
+                <td><input name = "location_name" type = "text"
+                           id = "location_name"></td>
+            </tr>
+
+            <tr>
+                <td width = "100"> </td>
+                <td> </td>
+            </tr>
+
+            <tr>
+                <td width = "100"> </td>
+                <td>
+                    <input name = "update" type = "submit"
+                           id = "update" value = "Update">
+                </td>
+            </tr>
+
+        </table>
+    </form>
+    <?php
 }
 
-mysqli_close($conn);
-
 ?>
-
-<form method = "post" action = "<?php $_PHP_SELF ?>">
-    <table width = "400" border =" 0" cellspacing = "1"
-           cellpadding = "2">
-
-        <tr>
-            <td width = "100">Employee ID</td>
-            <td><input name = "location_id" type = "text"
-                       id = "location_id"></td>
-        </tr>
-
-        <tr>
-            <td width = "100">Employee Salary</td>
-            <td><input name = "location_name" type = "text"
-                       id = "location_name"></td>
-        </tr>
-
-        <tr>
-            <td width = "100"> </td>
-            <td> </td>
-        </tr>
-
-        <tr>
-            <td width = "100"> </td>
-            <td>
-                <input name = "update" type = "submit"
-                       id = "update" value = "Update">
-            </td>
-        </tr>
-
-    </table>
-</form>
 
 </body>
 </html>
