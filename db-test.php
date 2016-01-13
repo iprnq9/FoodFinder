0ca47c5a25ee2bff2bc39013539a6c5806688241
Ian <ian.roberts@mst.edu>
2016-01-12 20:48:23 -0600

    Testing sql

--- db-test.php
+++ db-test.php
@@ -12,6 +12,8 @@ $username = "root";
 $password = "FoodFinder";
 $dbname = "foodfinders";
 
+$location_id = 1;
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
@@ -21,7 +23,7 @@ if (!$conn)
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT location_id, location_name FROM locations";
+$sql = "SELECT location_id, location_name FROM locations WHERE location_id = $location_id";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0)
@@ -44,49 +46,6 @@ mysqli_close($conn);
 
 ?>
 
-<br><br>
-
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
-ini_set('html_errors', 'On');
-
-$servername = "localhost";
-$username = "root";
-$password = "FoodFinder";
-$dbname = "foodfinders";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$conn)
-{
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "SELECT * FROM newlocations";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0)
-{
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result))
-  {
-    echo "<div class=\"card\">\n";
-    echo "<h1>ID: " . $row["location_id"]. "<br>Closes: " . $row["dinner_end"]. "</h1>\n";
-    echo "</div>";
-  }
-}
-
-else
-{
-  echo "0 results";
-}
-
-mysqli_close($conn);
-
-?>
 
 </body>
 </html>
