aac695ac2f3d26de363b542081cf6522dfb497c4
Ian <ian.roberts@mst.edu>
2016-01-19 18:50:00 -0600

    test

---
+++ update-paragraph.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+ini_set('html_errors', 'On');
+
+if(isset($_POST['update'])) {
+  $servername = "localhost";
+  $username = "root";
+  $password = "FoodFinder";
+  $dbname = "foodfinders";
+
+  $location_id = $_POST['location_id'];
+  $text = $_POST['text'];
+
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+  $sql = "INSERT INTO paragraphs (id, paragraph) VALUES ($location_id, \"$text\")";
+  $result = mysqli_query($conn, $sql);
+
+  if (!$result)
+  {
+    echo ('Could not update data.');
+  }
+
+  else
+  {
+    echo "Added successfully!";
+  }
+
+  mysqli_close($conn);
+}
+
+else
+{
+  ?>
+  <h1>Add new location:</h1>
+  <form method = "post" action = "<?php $_PHP_SELF ?>">
+    <table width = "400" border =" 0" cellspacing = "1"
+           cellpadding = "2">
+
+      <tr>
+        <td width = "100">Location ID</td>
+        <td><input name = "location_id" type = "text"
+                   id = "location_id"></td>
+      </tr>
+
+      <tr>
+        <td width = "300" height="400">Text</td>
+        <td><input name = "text" type = "text"
+                   id = "text"></td>
+      </tr>
+      
+      <tr>
+        <td width = "100"> </td>
+        <td>
+          <input name = "update" type = "submit"
+                 id = "update" value = "Update">
+        </td>
+      </tr>
+
+    </table>
+  </form>
+  <?php
+}
+
+?>
+</body>
+</html>
