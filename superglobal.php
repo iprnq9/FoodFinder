b7303b80b323bb8012dc88d3788abc48fdfd0631
Ian <ian.roberts@mst.edu>
2016-01-28 21:26:49 -0600

    Testing superglobal to use on profile page

---
+++ superglobal.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Superglobal Test | FoodFinder</title>
+</head>
+<body>
+
+<?php
+echo filter_input(INPUT_GET, 'profile-name');
+?>
+
+</body>
+</html>
