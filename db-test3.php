d051a32104df8a324d250ba66d41aac841ee03cd
Ian <ian.roberts@mst.edu>
2016-02-05 14:51:32 -0600

    Testing

---
+++ db-test3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+ini_set('html_errors', 'On');
+
+$servername = "localhost";
+$username = "root";
+$password = "FoodFinder";
+$dbname = "foodfinders";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn)
+{
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$name = $mysqli->query("SELECT location_name FROM locations WHERE locatin_id = 1")->fetch_object()->location_name;
+
+echo $name;
+
+mysqli_close($conn);
+
+?>
+
+
+</body>
+</html>
