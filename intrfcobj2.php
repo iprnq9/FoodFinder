16fcd47cb59aa0b0f1a19e09c7d3192ec6ec475e
hkaous <hkaous07@gmail.com>
2016-02-06 14:00:41 -0600

    change is a good thing
    change

--- intrfcobj2.php
+++ intrfcobj2.php
@@ -9,8 +9,8 @@ ini_set('html_errors', 'On');
 $host="localhost";
 $port=3306;
 $socket="";
-$user="joemnr";
-$password="engineer";
+$user="root";
+$password="FoodFinder";
 $dbname="goldgreen";
 
 $con = new mysqli($host, $user, $password, $dbname);
