805798ee15001b408faf79899ae0d4c0232737dd
hkaous <hkaous07@gmail.com>
2016-02-11 21:56:55 -0600

    safdafsdfasdf

--- intrfcobj.php
+++ intrfcobj.php
@@ -28,7 +28,7 @@ if ($con->connect_errno) {
 else {
     $opentime = $con->query("SELECT opentime FROM breakfast WHERE id=6 AND day = 5")->fetch_object()->opentime;
     $this = $opentime->num_rows;
-    echo $this;
+   // echo $this;
   //  $tj->setopnTime(1,"brkfst",$opentime);
     /* ->fetch_object()->opentime
  * for ($x = 0; $x < 7; $x++  ){
