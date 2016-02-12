4200a0fefae0bdf84a51198ce1173b6ce9d350e4
hkaous <hkaous07@gmail.com>
2016-02-11 21:46:23 -0600

    ll

--- intrfcobj.php
+++ intrfcobj.php
@@ -26,10 +26,12 @@ if ($con->connect_errno) {
 }
 
 else {
-    $opentime = $con->query("SELECT opentime FROM breakfast WHERE id=6 AND day = 5")->fetch_object()->opentime;
-    echo $opentime;
-    $tj->setopnTime(1,"brkfst",$opentime);
-/*    for ($x = 0; $x < 7; $x++  ){
+    $opentime = $con->query("SELECT opentime FROM breakfast WHERE id=6 AND day = 5");
+    $this = $opentime->num_rows;
+    echo $this;
+  //  $tj->setopnTime(1,"brkfst",$opentime);
+    /* ->fetch_object()->opentime
+ * for ($x = 0; $x < 7; $x++  ){
         $opentime = $con->query("SELECT opentime FROM breakfast WHERE id=6 AND day =$x");
         if(!empty($opentime)){           
             $opentime->fetch_object()->opentime;
@@ -39,7 +41,7 @@ else {
      
 }*/
 }
-echo $tj->getopnTime(1,"brkfst");
+// echo $tj->getopnTime(1,"brkfst");
 
 ?>
 </body>
