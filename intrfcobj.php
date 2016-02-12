97ecab802b5149a36e3365cacada14c8584754ef
hkaous <hkaous07@gmail.com>
2016-02-11 22:13:17 -0600

    ab

--- intrfcobj.php
+++ intrfcobj.php
@@ -41,8 +41,8 @@ else {
         $opentime = $con->query("SELECT opentime FROM breakfast WHERE id=6 AND day =$x");
         $row_cnt = $opentime->num_rows;
         if($row_cnt > 0)){
-            $opentime = $con->query("SELECT opentime FROM breakfast WHERE id=6 AND day =$x")->fetch_object()->opentime;
-            $tj->setopnTime($x,"brkfst",$opentime);
+            $opentime1 = $con->query("SELECT opentime FROM breakfast WHERE id=6 AND day =$x")->fetch_object()->opentime;
+            $tj->setopnTime($x,"brkfst",$opentime1);
         }
 
      
