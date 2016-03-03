bb405bb9d4c625d94d3b5b3be8f8a81a2a4134d2
hassan kaous <hassan kaous>
2016-03-02 19:38:31 -0600

    no message

--- hktest.php
+++ hktest.php
@@ -59,7 +59,7 @@ else {
     //get brkfst times
     for($x=0; $x < $count; $x++){
         $curid = $objArray[$x]->getId();
-        for ($y = 1; $y < 7; $y++  ){
+        for ($y = 1; $y <= 7; $y++  ){
             $opentime = $con->query("SELECT opentime FROM breakfast WHERE id=$curid AND day =$y");
             $row_cnt = $opentime->num_rows;
                 if($row_cnt > 0){
@@ -72,7 +72,7 @@ else {
     //lunch times
     for($x=0; $x < $count; $x++){
         $curid = $objArray[$x]->getId();
-        for ($y = 1; $y < 7; $y++  ){
+        for ($y = 1; $y <= 7; $y++  ){
             $opentime = $con->query("SELECT opentime FROM lunch WHERE id=$curid AND day =$y");
             $row_cnt = $opentime->num_rows;
                 if($row_cnt > 0){
@@ -85,7 +85,7 @@ else {
     //dinner times
     for($x=0; $x < $count; $x++){
         $curid = $objArray[$x]->getId();
-        for ($y = 1; $y < 7; $y++  ){
+        for ($y = 1; $y <= 7; $y++  ){
             $opentime = $con->query("SELECT opentime FROM dinner WHERE id=$curid AND day =$y");
             $row_cnt = $opentime->num_rows;
                 if($row_cnt > 0){
@@ -99,7 +99,7 @@ else {
     //get brkfst close times
     for($x=0; $x < $count; $x++){
         $curid = $objArray[$x]->getId();
-        for ($y = 1; $y < 7; $y++  ){
+        for ($y = 1; $y <= 7; $y++  ){
             $closetime = $con->query("SELECT closetime FROM breakfast WHERE id=$curid AND day =$y");
             $row_cnt = $closetime->num_rows;
             if($row_cnt > 0){
@@ -112,7 +112,7 @@ else {
     //get lunch close times
     for($x=0; $x < $count; $x++){
         $curid = $objArray[$x]->getId();
-        for ($y = 1; $y < 7; $y++  ){
+        for ($y = 1; $y <= 7; $y++  ){
             $closetime = $con->query("SELECT closetime FROM lunch WHERE id=$curid AND day =$y");
             $row_cnt = $closetime->num_rows;
             if($row_cnt > 0){
@@ -126,7 +126,7 @@ else {
     //get dinner close times
     for($x=0; $x < $count; $x++){
         $curid = $objArray[$x]->getId();
-        for ($y = 1; $y < 7; $y++  ){
+        for ($y = 1; $y <= 7; $y++  ){
             $closetime = $con->query("SELECT closetime FROM dinner WHERE id=$curid AND day =$y");
             $row_cnt = $closetime->num_rows;
             if($row_cnt > 0){
@@ -142,23 +142,23 @@ else {
 
 //for testing
 $max = sizeof($objArray);
-for($i = 0; $i < ($max-1) ; $i++){
+for($i = 0; $i < ($max) ; $i++){
     echo "Location Name: " . $objArray[$i]->getName() ."<br>";
     echo "Location ID: " . $objArray[$i]->getId() ."<br>";
     echo "Current status: " . $objArray[$i]->status() ."<br>";
-    for ($x = 1; $x < 7; $x++  ){
+    for ($x = 1; $x <= 7; $x++  ){
         $this20 = $objArray[$i]->getopnTime($x,"brkfst");
         if( $this20 != Null){
             echo "breakfast " . $x . " : ". $objArray[$i]->getopnTime($x,"brkfst") ."<br>";
      }}
 
-     for ($x = 1; $x < 7; $x++  ){
+     for ($x = 1; $x <= 7; $x++  ){
         $this20 = $objArray[$i]->getopnTime($x,"lnch");
         if( $this20 != Null){
             echo "Lunch " . $x . " : ". $objArray[$i]->getopnTime($x,"lnch") ."<br>";
      }}
 
-     for ($x = 1; $x < 7; $x++  ){
+     for ($x = 1; $x <= 7; $x++  ){
         $this20 = $objArray[$i]->getopnTime($x,"dnnr");
         if( $this20 != Null){
             echo "Dinner " . $x . " : ". $objArray[$i]->getopnTime($x,"dnnr") ."<br>";
