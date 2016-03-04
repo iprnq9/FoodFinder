193f4cf2c6cb8cf2b7327fd30edaa41c4a911481
Ian <ian.roberts@mst.edu>
2016-03-03 19:48:46 -0600

    Added JS files to irtest

--- irtest.php
+++ irtest.php
@@ -15,7 +15,9 @@
 <script type="text/javascript" src="js/moment.js"></script>
 <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700,500' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Oswald:700,300,400' rel='stylesheet' type='text/css'>
-
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
 <body>
 <?php
 include 'food-finderprj.php';
