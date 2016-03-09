34e3696576060903bdd090c52515510978f27308
Ian <iprnq9@mst.edu>
2016-03-09 14:07:30 -0600

    making progress

--- hours-einsteins.php
+++ hours-einsteins.php
@@ -1,42 +1,52 @@
-<table class="responsive-table centered bordered white">
-  <thead><tr>
-    <th>Day</th>
-    <th>Open</th>
-    <th>Closed</th>
-  </tr></thead>
-  <tr class="day-1">
-    <td>Monday</td>
-    <td>7:00am</td>
-    <td>7:00pm</td>
-  </tr>
-  <tr class="day-2">
-    <td>Tuesday</td>
-    <td>7:00am</td>
-    <td>7:00pm</td>
-  </tr>
-  <tr class="day-3">
-    <td>Wednesday</td>
-    <td>7:00am</td>
-    <td>7:00pm</td>
-  </tr>
-  <tr class="day-4">
-    <td>Thursday</td>
-    <td>7:00am</td>
-    <td>7:00pm</td>
-  </tr>
-  <tr class="day-5">
-    <td>Friday</td>
-    <td>7:00am</td>
-    <td>7:00pm</td>
-  </tr>
-  <tr class="day-6">
-    <td>Saturday</td>
-    <td>7:00am</td>
-    <td>7:00pm</td>
-  </tr>
-  <tr class="day-0">
-    <td>Sunday</td>
-    <td>7:00am</td>
-    <td>7:00pm</td>
-  </tr>
-</table>
+<?php
+
+echo "<table class=\"responsive-table centered bordered white\">\n";
+echo "  <thead><tr>\n";
+echo "    <th>Day</th>\n";
+echo "    <th>Open</th>\n";
+echo "    <th>Closed</th>\n";
+echo "  </tr></thead>\n";
+
+  $locationId = 1;
+  for($i = 0; $i < 7; $i++) {
+
+  }
+
+echo "  <tr class=\"day-1\">\n";
+echo "    <td>Monday</td>\n";
+echo "    <td>7:00am</td>\n";
+echo "    <td>7:00pm</td>\n";
+echo "  </tr>\n";
+echo "  <tr class=\"day-2\">\n";
+echo "    <td>Tuesday</td>\n";
+echo "    <td>7:00am</td>\n";
+echo "    <td>7:00pm</td>\n";
+echo "  </tr>\n";
+echo "  <tr class=\"day-3\">\n";
+echo "    <td>Wednesday</td>\n";
+echo "    <td>7:00am</td>\n";
+echo "    <td>7:00pm</td>\n";
+echo "  </tr>\n";
+echo "  <tr class=\"day-4\">\n";
+echo "    <td>Thursday</td>\n";
+echo "    <td>7:00am</td>\n";
+echo "    <td>7:00pm</td>\n";
+echo "  </tr>\n";
+echo "  <tr class=\"day-5\">\n";
+echo "    <td>Friday</td>\n";
+echo "    <td>7:00am</td>\n";
+echo "    <td>7:00pm</td>\n";
+echo "  </tr>\n";
+echo "  <tr class=\"day-6\">\n";
+echo "    <td>Saturday</td>\n";
+echo "    <td>7:00am</td>\n";
+echo "    <td>7:00pm</td>\n";
+echo "  </tr>\n";
+echo "  <tr class=\"day-0\">\n";
+echo "    <td>Sunday</td>\n";
+echo "    <td>7:00am</td>\n";
+echo "    <td>7:00pm</td>\n";
+echo "  </tr>\n";
+echo "</table>\n";
+
+?>
