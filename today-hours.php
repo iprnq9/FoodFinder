2d76c8c21873ba01b71769a48e54407490f709f4
Ian <iprnq9@mst.edu>
2016-03-08 23:32:23 -0600

    Hmmm

--- today-hours.php
+++ today-hours.php
@@ -1,41 +1,20 @@
-<!--Thanks to Russell Bell for his help-->
-<!DOCTYPE html>
-<html>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-
-<title>S&T Dining | FoodFinder</title>
-
-<!-- CSS  -->
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="custom.css" rel="stylesheet">
-<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-<script type="text/javascript" src="js/moment.js"></script>
-<link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700,500' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Oswald:700,300,400' rel='stylesheet' type='text/css'>
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
-<script src="js/materialize.js"></script>
-<script src="js/init.js"></script>
-<body>
 <?php
-include 'food-finderprj.php';
-
-include 'db-connect.php';
-
-$con = new mysqli($host, $user, $password, $dbname);
-
-$objArray = array();
-
-$dayNumber = date("w")+1;
-
-if ($con->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
-}
-
-else {
-    include 'pullData.php';
+//include 'food-finderprj.php';
+//
+//include 'db-connect.php';
+//
+//$con = new mysqli($host, $user, $password, $dbname);
+//
+//$objArray = array();
+//
+//$dayNumber = date("w")+1;
+//
+//if ($con->connect_errno) {
+//    echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+//}
+//
+//else {
+//    include 'pullData.php';
 
     $sinceEpoch = strtotime("today");
 
@@ -80,10 +59,8 @@ else {
 
         echo "            </table>";
         echo "          </p>";
-    //}
-
-}
+//    //}
+//
+//}
 
 ?>
-</body>
-</html>
