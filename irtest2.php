bfc2866eb9858d784186cd690b882d6277e34279
Ian <iprnq9@mst.edu>
2016-03-09 20:57:35 -0600

    Testing

---
+++ irtest2.php
@@ -0,0 +1,91 @@
+<!--Thanks to Russell Bell for his help-->
+<!DOCTYPE html>
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+
+<title>S&T Dining | FoodFinder</title>
+
+<!-- CSS  -->
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="custom.css" rel="stylesheet">
+<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+<script type="text/javascript" src="js/moment.js"></script>
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700,500' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Oswald:700,300,400' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
+<body>
+<?php
+include 'food-finderprj2.php';
+
+include 'header.php';
+
+include 'db-connect.php';
+
+$con = new mysqli($host, $user, $password, $dbname);
+
+$objArray = array();
+
+if ($con->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+}
+
+else {
+    include 'pullData.php';
+
+    $sinceEpoch = strtotime("today");
+
+    $mealArray = array("brkfst", "lnch", "dnnr");
+    $numOfMeals = sizeof($mealArray);
+
+    echo '<div class="container"><div style="text-align: center;">';
+    include 'currently.php';
+    echo '</div><div class="section"><ul class="flex-container">';
+
+
+        $imageClass = "imageClass-" . $objArray[$k]->getId();
+        echo "      <li class=\"flex-item card\">\n";
+        echo "        <div class=\"card-status " . $objArray[$k]->status() . "\">" . $objArray[$k]->status() . "</div>\n";
+        echo "        <div class=\"card-image " . $imageClass . "\"></div>\n";
+        echo "        <div class=\"card-info\">\n";
+        echo "          <p class=\"card-title\">" . $objArray[$k]->getName() . "</p>\n";
+        echo "          <p class=\"card-subtitle\">Description here...</p>\n";
+        //echo "          <br>\n";
+
+
+
+
+        echo "<p class=\"card-hours center-align\"><span class=\"todays-hours-text\">Today's Hours <i class=\"material-icons\">schedule</i></span>";
+        echo "            <table class=\"table centered bordered white\" style=\"width: 50%;margin: 0 auto;\">";
+        echo "            <thead><tr>";
+        echo "              <th>Open</th>";
+        echo "              <th>-</th>";
+        echo "              <th>Closed</th>";
+        echo "            </tr></thead>";
+
+        for ($i = 0; $i < ($objArray[0]->getNumOpenCloseTimes); $i++) {
+            echo '<tr><td>' . $objArray[0]->getOpenTime($i) . '</td><td>-</td><td>' . $objArray[0]->getCloseTimes($i) . '</td></tr>';
+        }
+        echo "            </table>";
+        echo "          </p>";
+        echo "          <div class=\"profile-button\"><a href=\"material-profile.php\" class=\"waves-effect waves-light btn green center-align z-depth-2\"><i class=\"material-icons left\">person_pin</i>View Profile</a></div>\n";
+        echo "        </div>\n";
+        echo "      </li>";
+
+    echo "</ul></div></div>";
+}
+
+include 'footer.php';
+
+?>
+
+<script>
+    $("li.dining").addClass("active");
+</script>
+
+</body>
+</html>
--- pullData.php
+++ pullData.php
@@ -2,98 +2,101 @@
 //get the number of objects in database
 $resource = $con->query("SELECT * FROM location_id");
 $count = $resource->num_rows;
+$dayNumber = date("w")+1;
 
 //create objects in array
-for($x=0; $x < $count; $x++){
-$objArray[$x] = new joeMinr;
+for ($x = 0; $x < $count; $x++) {
+    $objArray[$x] = new joeMinr;
 }
 
 //assign names to objects || set ids
 $nameC = $count+1;
-for ($x = 1; $x < $nameC; $x++  ){
-$location_name = $con->query("SELECT location_name FROM location_id WHERE id=$x")->fetch_object()->location_name;
-$y = $x-1;
-$objArray[$y]->setName($location_name);
-$objArray[$y]->setId($x);
+for ($x = 1; $x < $nameC; $x++) {
+    $location_name = $con->query("SELECT location_name FROM location_id WHERE id=$x")->fetch_object()->location_name;
+    $y = $x - 1;
+    $objArray[$y]->setName($location_name);
+    $objArray[$y]->setId($x);
+    $objArray[$y]->setOpenCloseArray($dayNumber);
 }
 
 //OPEN TIMES
 //get brkfst times
-for($x=0; $x < $count; $x++){
-$curid = $objArray[$x]->getId();
-for ($y = 1; $y < 7; $y++  ){
-$opentime = $con->query("SELECT opentime FROM breakfast WHERE id=$curid AND day =$y");
-$row_cnt = $opentime->num_rows;
-if($row_cnt > 0){
-$opentime = $con->query("SELECT opentime FROM breakfast WHERE id=$curid AND day =$y")->fetch_object()->opentime;
-$objArray[$x]->setopnTime($y,"brkfst",$opentime);
-}
-}
+for ($x = 0; $x < $count; $x++) {
+    $curid = $objArray[$x]->getId();
+    for ($y = 1; $y < 7; $y++) {
+        $opentime = $con->query("SELECT opentime FROM breakfast WHERE id=$curid AND day =$y");
+        $row_cnt = $opentime->num_rows;
+        if ($row_cnt > 0) {
+            $opentime = $con->query("SELECT opentime FROM breakfast WHERE id=$curid AND day =$y")->fetch_object()->opentime;
+            $objArray[$x]->setopnTime($y, "brkfst", $opentime);
+        }
+    }
 }
 
 //lunch times
-for($x=0; $x < $count; $x++){
-$curid = $objArray[$x]->getId();
-for ($y = 1; $y < 7; $y++  ){
-$opentime = $con->query("SELECT opentime FROM lunch WHERE id=$curid AND day =$y");
-$row_cnt = $opentime->num_rows;
-if($row_cnt > 0){
-$opentime = $con->query("SELECT opentime FROM lunch WHERE id=$curid AND day =$y")->fetch_object()->opentime;
-$objArray[$x]->setopnTime($y,"lnch",$opentime);
-}
-}
+for ($x = 0; $x < $count; $x++) {
+    $curid = $objArray[$x]->getId();
+    for ($y = 1; $y < 7; $y++) {
+        $opentime = $con->query("SELECT opentime FROM lunch WHERE id=$curid AND day =$y");
+        $row_cnt = $opentime->num_rows;
+        if ($row_cnt > 0) {
+            $opentime = $con->query("SELECT opentime FROM lunch WHERE id=$curid AND day =$y")->fetch_object()->opentime;
+            $objArray[$x]->setopnTime($y, "lnch", $opentime);
+        }
+    }
 }
 
 //dinner times
-for($x=0; $x < $count; $x++){
-$curid = $objArray[$x]->getId();
-for ($y = 1; $y < 7; $y++  ){
-$opentime = $con->query("SELECT opentime FROM dinner WHERE id=$curid AND day =$y");
-$row_cnt = $opentime->num_rows;
-if($row_cnt > 0){
-$opentime = $con->query("SELECT opentime FROM dinner WHERE id=$curid AND day =$y")->fetch_object()->opentime;
-$objArray[$x]->setopnTime($y,"dnnr",$opentime);
-}
-}
+for ($x = 0; $x < $count; $x++) {
+    $curid = $objArray[$x]->getId();
+    for ($y = 1; $y < 7; $y++) {
+        $opentime = $con->query("SELECT opentime FROM dinner WHERE id=$curid AND day =$y");
+        $row_cnt = $opentime->num_rows;
+        if ($row_cnt > 0) {
+            $opentime = $con->query("SELECT opentime FROM dinner WHERE id=$curid AND day =$y")->fetch_object()->opentime;
+            $objArray[$x]->setopnTime($y, "dnnr", $opentime);
+        }
+    }
 }
 
 //CLOSETIMES
 //get brkfst close times
 for($x=0; $x < $count; $x++){
-$curid = $objArray[$x]->getId();
-for ($y = 1; $y < 7; $y++  ){
-$closetime = $con->query("SELECT closetime FROM breakfast WHERE id=$curid AND day =$y");
-$row_cnt = $closetime->num_rows;
-if($row_cnt > 0){
-$closetime = $con->query("SELECT closetime FROM breakfast WHERE id=$curid AND day =$y")->fetch_object()->closetime;
-$objArray[$x]->setclsTime($y,"brkfst",$closetime);
-}
-}
+    $curid = $objArray[$x]->getId();
+    for ($y = 1; $y < 7; $y++  ){
+        $closetime = $con->query("SELECT closetime FROM breakfast WHERE id=$curid AND day =$y");
+        row_cnt = $closetime->num_rows;
+        if($row_cnt > 0){
+            $closetime = $con->query("SELECT closetime FROM breakfast WHERE id=$curid AND day =$y")->fetch_object()->closetime;
+            $objArray[$x]->setclsTime($y,"brkfst",$closetime);
+        }
+    }
 }
 
 //get lunch close times
 for($x=0; $x < $count; $x++){
-$curid = $objArray[$x]->getId();
-for ($y = 1; $y < 7; $y++  ){
-$closetime = $con->query("SELECT closetime FROM lunch WHERE id=$curid AND day =$y");
-$row_cnt = $closetime->num_rows;
-if($row_cnt > 0){
-$closetime = $con->query("SELECT closetime FROM lunch WHERE id=$curid AND day =$y")->fetch_object()->closetime;
-$objArray[$x]->setclsTime($y,"lnch",$closetime);
-}
-}
+    $curid = $objArray[$x]->getId();
+    for ($y = 1; $y < 7; $y++  ){
+        $closetime = $con->query("SELECT closetime FROM lunch WHERE id=$curid AND day =$y");
+        $row_cnt = $closetime->num_rows;
+        if($row_cnt > 0){
+            $closetime = $con->query("SELECT closetime FROM lunch WHERE id=$curid AND day =$y")->fetch_object()->closetime;
+            $objArray[$x]->setclsTime($y,"lnch",$closetime);
+        }
+    }
 }
 
 //get dinner close times
 for($x=0; $x < $count; $x++){
-$curid = $objArray[$x]->getId();
-for ($y = 1; $y < 7; $y++  ){
-$closetime = $con->query("SELECT closetime FROM dinner WHERE id=$curid AND day =$y");
-$row_cnt = $closetime->num_rows;
-if($row_cnt > 0){
-$closetime = $con->query("SELECT closetime FROM dinner WHERE id=$curid AND day =$y")->fetch_object()->closetime;
-$objArray[$x]->setclsTime($y,"dnnr",$closetime);
+    $curid = $objArray[$x]->getId();
+    for ($y = 1; $y < 7; $y++  ){
+        $closetime = $con->query("SELECT closetime FROM dinner WHERE id=$curid AND day =$y");
+        $row_cnt = $closetime->num_rows;
+        if($row_cnt > 0){
+            $closetime = $con->query("SELECT closetime FROM dinner WHERE id=$curid AND day =$y")->fetch_object()->closetime;
+            $objArray[$x]->setclsTime($y,"dnnr",$closetime);
+        }
+    }
 }
-}
-}
+
 ?>
