b211c03e20e3022729372f25afd04f7d12446e7d
Ian <iprnq9@mst.edu>
2016-03-12 21:38:05 -0600

    Tesgin

--- irtest4.php
+++ irtest4.php
@@ -26,7 +26,7 @@ else {
     {
         echo "Name: " . $objArray[$location]->getName();
         echo "<br><br>";
-        echo "getopnTime(6, \"lnch\"): " . $objArray[$location]->getopnTime(6, "lnch");
+        echo "getopnTime(6, \"brkfst\"): " . $objArray[$location]->getopnTime(6, "brkfst");
         echo "<br><br>";
         echo "Id: " . $objArray[$location]->getId();
         echo "<br><br>";
