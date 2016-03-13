eef776a9a0be5bdf6040b7999eb3c8181f02768a
Ian <iprnq9@mst.edu>
2016-03-13 00:52:45 -0600

    Here Wego

--- material-profile.php
+++ material-profile.php
@@ -17,71 +17,27 @@
 <body style="background-color: #eeeeee;">
 
 <?php
+
+include 'food-finderprj2.php';
+
+include 'db-connect.php';
+
+//get location id from URL variable
+$locationId = htmlspecialchars($_GET["id"]);
+$k = $locationId;
+$imageClass = "imageClass-" . $locationId;
+
+$con = new mysqli($host, $user, $password, $dbname);
+
+$objArray = array();
+
+$dayArray = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+$dayNumber = date("w")+1;
   include 'header.php';
 ?>
 
-<main>
-  <div class="container">
-    <div class="section">
-      <div class="row" style="text-align: center;">
-        <div class="profile-image imageClass-1"></div>
-        <div class="profile-name green z-depth-2">Einstein Bros Bagels</div>
-        <ul class="flex-container">
-          <li class="flex-item-wide z-depth-2">
-            <div class="quick-info green">
-              <div class="card-status open">OPEN</div>
-              <div class="card-content">
-                <h4 class="center-align" style="margin-top: -5px;">Hours of Operation</h4>
-                <h6 class="center-align week-of"></h6>
-                <?php include 'hours-einsteins.php' ?>
-              </div>
-            </div>
-          </li>
 
-          <li class="flex-item">
-            <div class="quick-info green z-depth-2">
-              <div class="card-image einsteins1"></div>
-              <div class="quick-info-text">
-                <h3>Our Mission</h3>
-                Einstein Bros. Bagels is your neighborhood bagel shop. We’re proud to provide our guests with freshly baked bagels, breakfast sandwiches, lunch sandwiches, coffee, catering and so much more. Stop on in. We’ll have a fresh bagel and cup of coffee ready for you.
-              </div>
-            </div>
-          </li>
-
-          <li class="flex-item">
-            <div class="quick-info green z-depth-2">
-              <div class="card-image einsteins3"></div>
-              <div class="quick-info-text">
-                <h3>Darn Good Coffee</h3>
-                We serve up your soon-to-be favorite drinks. Whether they be hot, cold, or iced, a cup of happiness awaits your hectic workday, relaxing day at home, or fun day out with friends.
-              </div>
-            </div>
-          </li>
-
-          <li class="flex-item">
-            <div class="quick-info green z-depth-2">
-              <div class="card-image einsteins2"></div>
-              <div class="quick-info-text">
-                <h3>More Than A Snack</h3>
-                 In addition to serving up delicious bagels and coffee, we cook gourmet breakfast and lunch sandwiches. High-five your tastebuds!
-              </div>
-            </div>
-          </li>
-
-          <li class="flex-item">
-            <div class="quick-info green z-depth-2">
-              <div class="card-image einsteins1"></div>
-              <div class="quick-info-text">
-                <h3>Download Menu</h3>
-                <a href="images/einstieins-menu.pdf" target="_blank"><img src="images/foodMenu.png" class="menu-download" alt="Download image"/></a>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</main>
 
   <?php include 'footer.php' ?>
 
