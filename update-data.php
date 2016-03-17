3e7b038a789a58e071d4f6850128f67bff3ad33a
Ian <iprnq9@mst.edu>
2016-03-16 21:22:10 -0500

    Testing

--- update-data.php
+++ update-data.php
@@ -30,11 +30,10 @@ if ($con->connect_errno) {
 }
 
 else {
-
-    if ($id < 1 || $id > $max || $id == NULL){
         echo "Choose a location to update: <select id=\"dynamic_select\">";
             for($i=1; $i <= $max; $i++){
-                echo "<option value=\"update-location.php?id=" . $i . "\">" . $objArray[$i-1]->getName() . "</option>";
+                echo "<option value=\"update-location.php?id=" . $i . "\">$i. " . $objArray[$i-1]->getName() .
+                    "</option>";
             }
         echo "</select>";
 
@@ -51,116 +50,7 @@ else {
         echo "</script>";
     }
 
-    else if(isset($_POST['update1'])) {
-
-        $location_name = $_POST['location_name'];
-
-        $sql = "UPDATE location_id SET location_name=\"$location_name\" WHERE id=$id";
-        $result = mysqli_query($con, $sql);
-
-        if (!$result)
-        {
-            echo ('Error: Could not update data.');
-        }
-
-        else
-        {
-            echo "Name updated successfully!";
-        }
-    }
-
-    else
-    {
-        ?>
-        <h1>Update existing location:&nbsp;<?php echo $objArray[$id-1]->getName(); ?></h1>
-        <h4>Update Name</h4>
-        <form method = "post" action = "<?php $_PHP_SELF ?>">
-            <table width = "400" border =" 0" cellspacing = "1"
-                   cellpadding = "2">
-
-                <tr>
-                    <td width = "100">New Name</td>
-                    <td><input name = "location_name" type = "text"
-                               id = "location_name"></td>
-                </tr>
-
-                <tr>
-                    <td width = "100"> </td>
-                    <td> </td>
-                </tr>
-
-                <tr>
-                    <td width = "100"> </td>
-                    <td>
-                        <input name = "update1" type = "submit"
-                               id = "update1" value = "Update Name">
-                    </td>
-                </tr>
-
-            </table>
-        </form>
-        <?php
-    }
-
-    ?>
-
-
-    <?php
-
-    if(isset($_POST['update2'])) {
-
-        $location_description = $_POST['location_description'];
-
-        $sql = "INSERT INTO descriptions (description) VALUES (\"$location_description\")";
-        $result = mysqli_query($con, $sql);
-
-        if (!$result)
-        {
-            echo ('Error: Could not update description.');
-        }
-
-        else
-        {
-            echo "Description updated successfully!";
-        }
-
-    }
-
-    else
-    {
-        ?>
-        <hr />
-        <h4>Update Description</h4>
-        <form method = "post" action = "<?php $_PHP_SELF ?>">
-            <table width = "600" border =" 0" cellspacing = "1"
-                   cellpadding = "2">
-
-                <tr>
-                    <td width = "100">New Description</td>
-                    <td><input style="width: 300px;" name = "location_description" type = "text"
-                               id = "location_description" placeholder="<?php echo $objArray[$id-1]->getDescription();
-                        ?>"></td>
-                </tr>
-
-                <tr>
-                    <td width = "100"> </td>
-                    <td> </td>
-                </tr>
-
-                <tr>
-                    <td width = "100"> </td>
-                    <td>
-                        <input name = "update2" type = "submit"
-                               id = "update2" value = "Update Description">
-                    </td>
-                </tr>
-
-            </table>
-        </form>
-        <?php
-    }
-}
-    ?>
+?>
 
 </body>
 </html>
