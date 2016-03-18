4122e6ad8e194fe9218f449e3be7356f0ef96e12
Ian <iprnq9@mst.edu>
2016-03-17 22:39:00 -0500

    Testing let's goooo!@@@@!!!

--- update-location2.php
+++ update-location2.php
@@ -22,9 +22,9 @@
 <body>
 <?php
 
-//error_reporting(E_ALL);
-//ini_set('display_errors', 'On');
-//ini_set('html_errors', 'On');
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+ini_set('html_errors', 'On');
 
 include 'food-finderprj2.php';
 
@@ -69,58 +69,33 @@ else {
     else
     {
         ?>
-    <h4 class="heading center-align"><i class="material-icons small">add_location</i>Add New Location</h4>
-<div class="row">
-    <ul class="collapsible popout" data-collapsible="expandable">
-        <li>
-            <form class="col s12 m12 l10 push-l1" method="post" action = "<?php $_PHP_SELF ?>">
-                <div class="collapsible-header green active"><i class="material-icons">store</i>Update Name</div>
-                <div class="collapsible-body grey darken-4 green-text">
-                    <div class="row" style="padding: 10px;">
-                        <div class="input-field col s12 m12">
-                            <i class="material-icons prefix">info_outline</i>
-                            <input placeholder='<?php $objArray[$id-1]->getName(); ?>' id="location_name" type="text"
-                            length="45"
-                            class="validate">
-                            <label for="location_name">Updated Name</label>
+        <h4 class="heading center-align"><i class="material-icons small">add_location</i>Add New Location</h4>
+        <div class="row">
+            <ul class="collapsible popout" data-collapsible="expandable">
+                <li>
+                    <form class="col s12 m12 l10 push-l1" method="post" action = "<?php $_PHP_SELF ?>">
+                        <div class="collapsible-header green active"><i class="material-icons">store</i>Update Name</div>
+                        <div class="collapsible-body grey darken-4 green-text">
+                            <div class="row" style="padding: 10px;">
+                                <div class="input-field col s12 m12">
+                                    <i class="material-icons prefix">info_outline</i>
+                                    <input placeholder='<?php echo $objArray[$id-1]->getName(); ?>' id="location_name"
+                                           type="text"
+                                    length="45"
+                                    class="validate">
+                                    <label for="location_name">Updated Name</label>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <button class="btn-large waves-effect waves-light green lighten-1" type="submit" name="update1"
-                        id="update1">Update Name
-                    <i class="material-icons right">send</i>
-                </button>
-            </form>
-
-            <form method = "post" action = "<?php $_PHP_SELF ?>">
-                <table width = "400" border =" 0" cellspacing = "1"
-                       cellpadding = "2">
-
-                    <tr>
-                        <td width = "100">New Name</td>
-                        <td><input name = "location_name" type = "text"
-                                   id = "location_name"></td>
-                    </tr>
-
-                    <tr>
-                        <td width = "100"> </td>
-                        <td> </td>
-                    </tr>
-
-                    <tr>
-                        <td width = "100"> </td>
-                        <td>
-                            <input name = "update1" type = "submit"
-                                   id = "update1" value = "Update Name">
-                        </td>
-                    </tr>
-
-                </table>
-            </form>
-        </li>
-    </ul>
-</div>
-
+                        <button class="btn-large waves-effect waves-light green lighten-1" type="submit" name="update1"
+                                id="update1">Update Name
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+<?php } ?>
 <?php
 }
 
