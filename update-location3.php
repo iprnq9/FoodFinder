c04b4e634c1851426dea901b9eebc75b1685b021
Ian <iprnq9@mst.edu>
2016-03-18 16:50:31 -0500

    TEsting image upload

--- update-location3.php
+++ update-location3.php
@@ -242,7 +242,8 @@ else {
             <div class="row" style="padding: 10px;">
                 <div class="input-field col s6">
                     <i class="material-icons prefix">label_outline</i>
-                    <textarea class="materialize-textarea" id="card1_paragraph" name="card1_paragraph" type="text" length="120" placeholder="Currently: <?php echo $objArray[$id-1]->getParagraph(0);  ?>"></textarea>
+                    <textarea class="materialize-textarea" id="card1_paragraph" name="card1_paragraph" type="text" length="300" placeholder="Currently: <?php echo
+                    $objArray[$id-1]->getParagraph(0);  ?>"></textarea>
                     <label for="card1_paragraph">New Card 1 Paragraph</label>
                 </div>
             </div>
@@ -256,7 +257,101 @@ else {
     }
     echo "</div></div></li>";
 
+    echo "            <li>\n";
+    echo "                <div class=\"collapsible-header green black-text active\">\n";
+    echo "                    <i class=\"material-icons\">store</i>Update Card 1 Paragraph\n";
+    echo "                </div>\n";
+    echo "                <div class=\"collapsible-body grey darken-4 green-text\">\n";
+    echo "                   <div class=\"row\"  style=\"padding: 10px;\">\n";
 
+    $target_dir = "images/" . $id . "/";
+    $target_file = $target_dir . basename($_FILES["card1_image"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    // Check if image file is a actual image or fake image
+    if(isset($_POST['update5'])) {
+        $check = getimagesize($_FILES["card1_image"]["tmp_name"]);
+        if ($check !== false) {
+            $uploadOk = 1;
+        }
+
+        else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+
+
+        // Check file size
+        if ($_FILES["card1_image"]["size"] > 1000000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+        // Allow certain file formats
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif") {
+            echo "Sorry, only JPG/JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+
+        }
+
+        // if everything is ok, try to upload file
+        else {
+            $filename = basename($_FILES["card1_image"]["name"]);
+            if (move_uploaded_file($_FILES["card1_image"]["tmp_name"], $target_file)) {
+                $sql = "UPDATE descriptions SET img1=\"" . $filename . "\" WHERE id=" . $id;
+                $result = mysqli_query($con, $sql);
+                //echo $sql;
+                //echo $filename;
+
+                if (!$result)
+                {
+                    echo ('Error: Could not update Card 1 Image.');
+                }
+
+                else
+                {
+                    echo "The file \"" . $filename . "\" has been uploaded.";
+                }
+
+            }
+            else {
+                echo "Sorry, there was an error uploading your file \"" . $filename . "\".";
+            }
+        }
+    }
+
+    else
+    { ?>
+        <form class="col s12" method="post" action="<?php $_PHP_SELF ?>" enctype="multipart/form-data">
+            <div class="row" style="padding: 10px;">
+                <div class="file-field input-field">
+                    <div class="btn">
+                        <span>Choose File</span>
+                        <input type="file" name="card1_image" id="card1_image" placeholder="<?php $objArray[$id-1]->getImage(0);?>">
+                    </div>
+                    <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text">
+                    </div>
+                </div>
+            </div>
+            <div class="input-field col s6">
+                <button class="btn waves-effect waves-light" type="submit" name="update5" id="update5">Update
+                    <i class="material-icons right">send</i>
+                </button>
+            </div>
+        </form>
+        <?php
+    }
+    echo "</div></div></li>";
+
+
+    ?>
+
+<?php
     echo "                   </ul>\n";
     //echo "                </div>\n";
     echo "            </li>\n";
