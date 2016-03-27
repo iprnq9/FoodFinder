4b01acd67af96bc21f97e7f531bb351ad2c7f9b8
Ian <iprnq9@mst.edu>
2016-03-27 13:02:13 -0500

    Added theme color for chrome mobile color

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+<meta name="theme-color" content="#66BB6A" />
 
 <title>S&T Dining | FoodFinder</title>
 
--- profile.php
+++ profile.php
@@ -3,6 +3,8 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <meta name="theme-color" content="#66BB6A" />
+
     <title><!--TITLE--> | FoodFinder</title>
 
     <!-- CSS  -->
