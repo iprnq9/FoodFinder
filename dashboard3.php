757f7407c22813af6d5673f2a15da38bb23e6292
Ian <iprnq9@mst.edu>
2016-04-15 14:39:54 -0500

    Name switch of dashboard.php

---
+++ dashboard3.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Dashboard | FoodFinder</title>
+
+  <!-- CSS  -->
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="custom.css" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <script type="text/javascript" src="js/moment.js"></script>
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700,500' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Oswald:700,300,400' rel='stylesheet' type='text/css'>
+
+</head>
+<body style="background-color: #eeeeee;zoom: 75%;">
+
+<div style="text-align: center;">
+  <div class="currently z-depth-1 green"></div>
+</div>
+
+<ul class="flex-container">
+      <li class="flex-item card">
+        <div class="card-status open einsteins-status">OPEN</div>
+        <div class="card-image einsteins"></div>
+        <div class="card-info">
+          <div class="card-title-subtitle">
+            <p class="card-title">Einstein Bros Bagels</p>
+            <span class="card-subtitle">A coffee shop serving delicious bagels and more.</span>
+          </div>
+            <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+              <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+            <thead><tr>
+              <th>Open</th>
+              <th>-</th>
+              <th>Closed</th>
+            </tr></thead>
+            <tr>
+              <td>7:00am</td>
+              <td>-</td>
+              <td>7:00pm</td>
+            </tr>
+            </table>
+          <div class="profile-button"><a href="material-profile.php" class="waves-effect waves-light btn green center-align z-depth-2"><i class="material-icons left">person_pin</i>View Profile</a></div>
+        </div>
+      </li>
+      <li class="flex-item card">
+        <div class="card-status closed">CLOSED</div>
+        <div class="card-image rustic"></div>
+        <div class="card-info">
+          <div class="card-title-subtitle">
+            <p class="card-title">Rustic Range</p>
+            <p class="card-subtitle">Classic burgers, made as desired.</p>
+          </div>
+            <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+              <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+            <thead><tr>
+              <th>Open</th>
+              <th>-</th>
+              <th>Closed</th>
+            </tr></thead>
+            <tr>
+              <td>10:45am</td>
+              <td>-</td>
+              <td>7:00pm</td>
+            </tr>
+            </table>
+          <div class="profile-button"><a href="material-profile.php" class="waves-effect waves-light btn green center-align z-depth-2"><i class="material-icons left">person_pin</i>View Profile</a></div>
+        </div>
+      </li>
+      <li class="flex-item card">
+        <div class="card-status open">OPEN</div>
+        <div class="card-image mondo"></div>
+        <div class="card-info">
+          <div class="card-title-subtitle">
+            <p class="card-title">Mondo Subs</p>
+            <p class="card-subtitle">Sandwiches served made to order.</p>
+          </div>
+          <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+            <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+            <thead><tr>
+              <th>Open</th>
+              <th>-</th>
+              <th>Closed</th>
+            </tr></thead>
+            <tr>
+              <td>7:00am</td>
+              <td>-</td>
+              <td>9:00pm</td>
+            </tr>
+          </table>
+          <div class="profile-button"><a href="material-profile.php" class="waves-effect waves-light btn green center-align z-depth-2"><i class="material-icons left">person_pin</i>View Profile</a></div>
+        </div>
+      </li>
+      <li class="flex-item card">
+        <div class="card-status open">OPEN</div>
+        <div class="card-image havener"></div>
+        <div class="card-info">
+          <div class="card-title-subtitle">
+            <p class="card-title">Havener Food Court</p>
+            <p class="card-subtitle">Italian, Mexican, Asian, Salad, or Homestyle. Options!</p>
+          </div>
+          <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+            <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+            <thead><tr>
+              <th>Open</th>
+              <th>-</th>
+              <th>Closed</th>
+            </tr></thead>
+            <tr>
+              <td>7:30am</td>
+              <td>-</td>
+              <td>2:00pm</td>
+            </tr>
+            <tr>
+              <td>4:30pm</td>
+              <td>-</td>
+              <td>7:00pm</td>
+            </tr>
+          </table>
+          <div class="profile-button"><a href="material-profile.php" class="waves-effect waves-light btn green center-align z-depth-2"><i class="material-icons left">person_pin</i>View Profile</a></div>
+        </div>
+      </li>
+      <li class="flex-item card">
+        <div class="card-status closed">CLOSED</div>
+        <div class="card-image patio"></div>
+        <div class="card-info">
+          <div class="card-title-subtitle">
+            <p class="card-title">The Patio</p>
+            <p class="card-subtitle">Mediterranean food to go!</p>
+          </div>
+          <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+            <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+            <thead><tr>
+              <th>Open</th>
+              <th>-</th>
+              <th>Closed</th>
+            </tr></thead>
+            <tr>
+              <td>10:30am</td>
+              <td>-</td>
+              <td>2:00pm</td>
+            </tr>
+          </table>
+          <div class="profile-button"><a href="material-profile.php" class="waves-effect waves-light btn green center-align z-depth-2"><i class="material-icons left">person_pin</i>View Profile</a></div>
+        </div>
+      </li>
+      <li class="flex-item card">
+        <div class="card-status closed">CLOSED</div>
+        <div class="card-image miner-munchies"></div>
+        <div class="card-info">
+          <div class="card-title-subtitle">
+            <p class="card-title">Miner Munchies</p>
+            <p class="card-subtitle">Come shop your favorite snacks and groceries!</p>
+          </div>
+          <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+            <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+            <thead><tr>
+              <th>Open</th>
+              <th>-</th>
+              <th>Closed</th>
+            </tr></thead>
+            <tr>
+              <td>5:00pm</td>
+              <td>-</td>
+              <td>10:00pm</td>
+            </tr>
+          </table>
+          <div class="profile-button"><a href="material-profile.php" class="waves-effect waves-light btn green center-align z-depth-2"><i class="material-icons left">person_pin</i>View Profile</a></div>
+        </div>
+      </li>
+        <li class="flex-item card">
+          <div class="card-status open einsteins-status">OPEN</div>
+          <div class="card-image miner-break"></div>
+          <div class="card-info">
+            <div class="card-title-subtitle">
+              <p class="card-title">Miner Break Caf&eacute;</p>
+              <span class="card-subtitle">Proudly serving Starbucks and more!</span>
+            </div>
+            <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+              <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+              <thead><tr>
+                <th>Open</th>
+                <th>-</th>
+                <th>Closed</th>
+              </tr></thead>
+              <tr>
+                <td>8:00am</td>
+                <td>-</td>
+                <td>7:00pm</td>
+              </tr>
+            </table>
+            <div class="profile-button"><a href="material-profile.php" class="waves-effect waves-light btn green center-align z-depth-2"><i class="material-icons left">person_pin</i>View Profile</a></div>
+          </div>
+        </li>
+      <li class="flex-item card">
+        <div class="card-status closing-soon">CLOSES IN 25 MIN</div>
+        <div class="card-image tj"></div>
+        <div class="card-info">
+          <div class="card-title-subtitle">
+            <p class="card-title">Thomas Jefferson Dining Hall</p>
+            <p class="card-subtitle">All you can eat buffet, with many options.</p>
+          </div>
+          <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+            <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+            <thead><tr>
+              <th>Open</th>
+              <th>-</th>
+              <th>Closed</th>
+            </tr></thead>
+            <tr>
+              <td>6:45am</td>
+              <td>-</td>
+              <td>10:30pm</td>
+            </tr>
+            <tr>
+              <td>10:45pm</td>
+              <td>-</td>
+              <td>2:00pm</td>
+            </tr>
+            <tr>
+              <td>4:45pm</td>
+              <td>-</td>
+              <td>7:30pm</td>
+            </tr>
+          </table>
+          <div class="profile-button"><a href="material-profile.php" class="waves-effect waves-light btn green center-align z-depth-2"><i class="material-icons left">person_pin</i>View Profile</a></div>
+        </div>
+      </li>
+      <li class="flex-item card">
+        <div class="card-status open">OPEN</div>
+        <div class="card-image bake"></div>
+        <div class="card-info">
+          <div class="card-title-subtitle">
+            <p class="card-title">IEEE Bake Sale</p>
+            <p class="card-subtitle">Help support us geeks by buying some treats!</p>
+          </div>
+          <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+            <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+            <thead><tr>
+              <th>Open</th>
+              <th>-</th>
+              <th>Closed</th>
+            </tr></thead>
+            <tr>
+              <td>7:45am</td>
+              <td>-</td>
+              <td>7:30pm</td>
+            </tr>
+          </table>
+          </div>
+      </li>
+      <li class="flex-item card">
+        <div class="card-status open">OPEN</div>
+        <div class="card-image soup"></div>
+        <div class="card-info">
+          <div class="card-title-subtitle">
+            <p class="card-title">Engineers Without Borders Potluck</p>
+            <p class="card-subtitle">Bring a soup and eat some soup!</p>
+          </div>
+          <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+            <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+            <thead><tr>
+              <th>Open</th>
+              <th>-</th>
+              <th>Closed</th>
+            </tr></thead>
+            <tr>
+              <td>10:00am</td>
+              <td>-</td>
+              <td>4:30pm</td>
+            </tr>
+          </table>
+          </div>
+      </li>
+      <li class="flex-item card">
+        <div class="card-status open">OPEN</div>
+        <div class="card-image donuts"></div>
+        <div class="card-info">
+          <div class="card-title-subtitle">
+            <p class="card-title">Kappa Delta Donut Sale</p>
+            <p class="card-subtitle">Help support us by buying a dozen!</p>
+          </div>
+          <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+            <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+            <thead><tr>
+              <th>Open</th>
+              <th>-</th>
+              <th>Closed</th>
+            </tr></thead>
+            <tr>
+              <td>8:00am</td>
+              <td>-</td>
+              <td>3:30pm</td>
+            </tr>
+          </table>
+          </div>
+      </li>
+      <li class="flex-item card">
+        <div class="card-status open">OPEN</div>
+        <div class="card-image bbq"></div>
+        <div class="card-info">
+          <div class="card-title-subtitle">
+            <p class="card-title">Pi Kappa Alpha Grill Out</p>
+            <p class="card-subtitle">Burgers, hot dogs, chips, and a drink.</p>
+          </div>
+          <table class="table centered bordered white" style="width: 50%;margin: 0 auto;">
+            <caption><p class="card-hours center-align"><span class="todays-hours-text">Today's Hours&nbsp;<i class="material-icons">schedule</i></span></p></caption>
+            <thead><tr>
+              <th>Open</th>
+              <th>-</th>
+              <th>Closed</th>
+            </tr></thead>
+            <tr>
+              <td>10:00am</td>
+              <td>-</td>
+              <td>2:30pm</td>
+            </tr>
+          </table>
+          </div>
+      </li>
+      </ul>
+
+<!--  Scripts-->
+  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  <script>
+    $(".currently").text("Monday" + ", " + "10:05am" + ": " + "Breakfast");
+    $(".currently").append("  <img src=\"images/logo-icon.png\" alt=\"FoodFinder logo image\" style=\"vertical-align: middle;\"/>&nbsp;Food Finder");
+  </script>
+
+
+
+</body>
+</html>
